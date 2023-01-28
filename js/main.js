4ad704658a2bfd1e6612ff4a0b98ecbe533f6a31
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-01-28 12:44:25 -0300

    Updating an item

--- js/main.js
+++ js/main.js
@@ -13,15 +13,22 @@ form.addEventListener("submit", (evento) => {
   const nome = evento.target.elements['nome']
   const quantidade = evento.target.elements['quantidade']
 
+  const existe = itens.find(elemento => elemento.nome === nome.value)
+
   const itemAtual = {
     'nome': nome.value,
     'quantidade': quantidade.value
   }
 
-  criaElemento(itemAtual)
-
-  itens.push(itemAtual)
-
+  if (existe) {
+    itemAtual.id = existe.id
+    atualizaElemento(itemAtual)
+    itens[existe.id] = itemAtual
+  } else {
+    itemAtual.id = itens.length
+    criaElemento(itemAtual)
+    itens.push(itemAtual)
+  }
   localStorage.setItem('itens', JSON.stringify(itens))
 
   nome.value = ""
@@ -35,10 +42,13 @@ function criaElemento(item) {
 
   const numeroItem = document.createElement('strong')
   numeroItem.innerHTML = item.quantidade
-
+  numeroItem.dataset.id = item.id
   novoItem.appendChild(numeroItem)
   novoItem.innerHTML += item.nome
 
   lista.appendChild(novoItem)
 }
 
+function atualizaElemento(item) {
+  document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
